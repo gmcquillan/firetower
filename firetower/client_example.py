566636736730a3c1718e95d2b7ccf1dabba974ef
Gavin <gavin.mcquillan@gmail.com>
2011-06-22 16:17:31 -0700

    Initial commit

---
+++ firetower/client_example.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import redis_util
+import simplejson as json
+import random
+
+queue_key = 'incoming'
+
+FAKE_SIGS = ["Test Error, Exception Blah",
+             "I'm broken!",
+             "Etc.",
+             "Yup!"
+]
+FAKE_DATA =  {"hostname": "testmachine",
+              "msg": "I/O Exception from some file",
+              "logfacility": None,
+              "syslogtag": None,
+              "programname": None,
+              "severity": None}
+
+class ClientExample(object):
+  """Main loop."""
+
+  def run(self):
+    queue = redis_util.Redis()
+    for i in xrange(0, 10000):
+      try:
+        # Semi-randomly seed the 'sig' key in our fake errors
+        FAKE_DATA['sig'] = random.choice(FAKE_SIGS)
+        encoded = json.dumps(FAKE_DATA)
+        err = queue.push(queue_key, encoded)
+      except:
+        print "Something went wrong storing value from redis"
+
+
+if __name__ == '__main__':
+  main = ClientExample()
+  main.run()
---
+++ firetower/display.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import pprint
+
+import redis_util
+
+
+class Display(object):
+
+  def __init__(self):
+    self.r = redis_util.Redis()
+
+  def dump_data(self, tracked_keys):
+    counts = self.r.get_counts(tracked_keys)
+    pprint.pprint(counts)
+    lastlogs = []
+    log_keys = self.r.conn.keys()
+    print "These are the log_keys"
+    pprint.pprint(log_keys)
+    for key in log_keys:
+      #Because we don't want to regurgitate the count keys
+      data_key = 'data_%' % (key,)
+      if data_key in tracked_keys:
+        last_hour = time.time() - datetime.timedelta(minutes=60)
+        error_data = self.r.conn.zrangebyscore(
+            data_key, last_hour, "inf", limit=1, withscores=True)
+        lastlogs.append(error_data)
+
+    pprint.pprint(lastlogs)
+
+if __name__ == '__main__':
+  display = Display()
+  display.dump_data(['Test Error'])
